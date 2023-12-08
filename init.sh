2d2094e1c58e2fc7e6e799e0c689856c41633af9
xboard <xboard@xboard.one>
2023-12-08 09:32:06 +0800

    fix: 修复webman环境下各种Content-Type为application/json 情况下无法获取请求数据的问题

--- init.sh
+++ init.sh
@@ -7,6 +7,7 @@ php composer.phar install -vvv
 php_main_version = $(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 1)
 if [ $major_version -ge 8 ]; then
     php composer.phar require joanhey/adapterman
+    php composer.phar require cedar2025/http-foundation:5.4.x-dev
 fi
 
 php artisan v2board:install
