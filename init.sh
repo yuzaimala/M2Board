2210139799054f13c2a2d2d690303dad6c89a602
wyx2685 <wyx2685@gmail.com>
2024-02-27 14:23:27 +0900

    Fix init.sh

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@ rm -rf composer.phar
 wget https://github.com/composer/composer/releases/latest/download/composer.phar -O composer.phar
 php composer.phar install -vvv
 
-php_main_version = $(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 1)
-if [ $major_version -ge 8 ]; then
+php_main_version=$(php -v | head -n 1 | cut -d ' ' -f 2 | cut -d '.' -f 1)
+if [ $php_main_version -ge 8 ]; then
     php composer.phar require joanhey/adapterman
     php composer.phar require cedar2025/http-foundation:5.4.x-dev
 fi
