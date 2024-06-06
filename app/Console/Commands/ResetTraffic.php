1a7b07468a4658a1ac7bf9c94523ace8c194b58e
wyx2685 <wyx2685@gmail.com>
2024-06-07 00:31:33 +0900

    Fix:有效期超过一年的流量重置BUG

--- app/Console/Commands/ResetTraffic.php
+++ app/Console/Commands/ResetTraffic.php
@@ -148,19 +148,20 @@ class ResetTraffic extends Command
         foreach ($builder->get() as $item) {
             $expireDay = date('d', $item->expired_at);
             $expireMonth = date('m', $item->expired_at);
+            $expireYear = date('Y', strtotime($item->expired_at));
             $today = date('d');
-
             $currentMonth = date('m');
+            $currentYear = date('Y');
 
             if ($expireDay === $today) {
-                if ($expireMonth == $currentMonth) {
+                if ($expireMonth == $currentMonth && $expireYear == $currentYear) {
                     continue;
                 }
                 array_push($users, $item->id);
             }
 
             if (($today === $lastDay) && $expireDay >= $lastDay) {
-                if ($expireMonth == $currentMonth) {
+                if ($expireMonth == $currentMonth && $expireYear == $currentYear) {
                     continue;
                 }
                 array_push($users, $item->id);
