1549cada0ca3f2062e8bde7c19426dd9434fe4c7
老衲不拈花 <32061921+lufeiit@users.noreply.github.com>
2024-09-04 12:59:41 +0800

    fix 去除代码中提示修改的中文字符，兼容不看字的小可爱 (#145)
    * - Stripe Sources API被弃用，更新为Payment Intents API
    
    - 支持 支付宝、微信、信用卡 （当然首先你的账户需要支持）
    
    - 增加推送用户邮箱及元数据到 Stripe
    
    - 增加 货币转换 API 到 3 个，并支持故障自动切换
    
    * fix 去除代码中提示修改的中文字符，兼容不看字的小可爱
    
    - 使用本扩展、composer.json中的stripe需更新到14.9。
    
    - 如使用 webman，nginx 伪静态中需配置绕过webhook payment order
    
    - 示例 
    `
    location ~ (/config/|/manage/|/webhook|/payment|/order|/theme/) {
    try_files $uri $uri/ /index.php$is_args$query_string;
    }
    `
    
    * fix 去除代码中提示修改的中文字符，兼容不看字的小可爱

--- app/Payments/StripeALL.php
+++ app/Payments/StripeALL.php
@@ -1,16 +1,10 @@
 <?php
 
-/**
- * 自己写别抄，抄NMB抄
- */
-
 namespace App\Payments;
-
 use Stripe\Stripe;
 use App\Models\User;
 
-class StripeALL
-{
+class StripeALL {
     public function __construct($config)
     {
         $this->config = $config;
@@ -41,7 +35,7 @@ class StripeALL
             ]
         ];
     }
-
+    
     public function pay($order)
     {
         $currency = $this->config['currency'];
@@ -55,87 +49,86 @@ class StripeALL
         $stripe = new \Stripe\StripeClient($this->config['stripe_sk_live']);
         // 获取用户邮箱
         $userEmail = $this->getUserEmail($order['user_id']);
-        if ($this->config['payment_method'] != "cards") {
-            $stripePaymentMethod = $stripe->paymentMethods->create([
-                'type' => $this->config['payment_method'],
-            ]);
-            // 准备支付意图的基础参数
-            $params = [
-                'amount' => floor($order['total_amount'] * $exchange),
-                'currency' => $currency,
-                'confirm' => true,
-                'payment_method' => $stripePaymentMethod->id,
-                'automatic_payment_methods' => ['enabled' => true],
-                'statement_descriptor' => '修改为你的订单描述英文前缀-#' . $order['user_id'] . '-' . substr($order['trade_no'], -8),
-                'metadata' => [
-                    'user_id' => $order['user_id'],
-                    'customer_email' => $userEmail,
-                    'out_trade_no' => $order['trade_no'],
-                    'identifier' => '修改为你的站点名称'
-                ],
-                'return_url' => $order['return_url']
-            ];
-
-            // 如果支付方式为 wechat_pay，添加相应的支付方式选项
-            if ($this->config['payment_method'] === 'wechat_pay') {
-                $params['payment_method_options'] = [
-                    'wechat_pay' => [
-                        'client' => 'web'
-                    ],
-                ];
-            }
-            //更新支持最新的paymentIntents方法，Sources API将在今年被彻底替
-            $stripeIntents = $stripe->paymentIntents->create($params);
-
-            $nextAction = null;
-
-            if (!$stripeIntents['next_action']) {
-                throw new abort(__('Payment gateway request failed'));
-            } else {
-                $nextAction = $stripeIntents['next_action'];
-            }
-
-            switch ($this->config['payment_method']) {
-                case "alipay":
-                    if (isset($nextAction['alipay_handle_redirect'])) {
-                        $jumpUrl = $nextAction['alipay_handle_redirect']['url'];
-                        $actionType = 1;
-                    } else {
-                        throw new abort('unable get Alipay redirect url', 500);
-                    }
-                    break;
-                case "wechat_pay":
-                    if (isset($nextAction['wechat_pay_display_qr_code'])) {
-                        $jumpUrl = $nextAction['wechat_pay_display_qr_code']['data'];
-                    } else {
-                        throw new abort('unable get WeChat Pay redirect url', 500);
-                    }
-            }
-        } else {
-            $creditCheckOut = $stripe->checkout->sessions->create([
-                'success_url' => $order['return_url'],
-                'client_reference_id' => $order['trade_no'],
-                'payment_method_types' => ['card'],
-                'line_items' => [
-                    [
-                        'price_data' => [
-                            'currency' => $currency,
-                            'unit_amount' => floor($order['total_amount'] * $exchange),
-                            'product_data' => [
-                                'name' => '修改为你的订单描述英文前缀-#' . $order['user_id'] . '-' . substr($order['trade_no'], -8),
-                            ]
-                        ],
-                        'quantity' => 1,
-                    ],
-                ],
-                'mode' => 'payment',
-                'invoice_creation' => ['enabled' => true],
-                'phone_number_collection' => ['enabled' => false],
+        if ($this->config['payment_method'] != "cards"){
+        $stripePaymentMethod = $stripe->paymentMethods->create([
+            'type' => $this->config['payment_method'],
+        ]);
+        // 准备支付意图的基础参数
+        $params = [
+            'amount' => floor($order['total_amount'] * $exchange),
+            'currency' => $currency,
+            'confirm' => true,
+            'payment_method' => $stripePaymentMethod->id,
+            'automatic_payment_methods' => ['enabled' => true],
+            'statement_descriptor' => 'user-#' . $order['user_id'] . '-' . substr($order['trade_no'], -8),
+            'metadata' => [
+                'user_id' => $order['user_id'],
                 'customer_email' => $userEmail,
-            ]);
-            $jumpUrl = $creditCheckOut['url'];
-            $actionType = 1;
+                'out_trade_no' => $order['trade_no']
+            ],
+            'return_url' => $order['return_url']
+        ];
+
+        // 如果支付方式为 wechat_pay，添加相应的支付方式选项
+        if ($this->config['payment_method'] === 'wechat_pay') {
+            $params['payment_method_options'] = [
+                'wechat_pay' => [
+                    'client' => 'web'
+                ],
+            ];
         }
+        //更新支持最新的paymentIntents方法，Sources API将在今年被彻底替
+        $stripeIntents = $stripe->paymentIntents->create($params);
+
+        $nextAction = null;
+        
+        if (!$stripeIntents['next_action']) {
+            throw new abort(__('Payment gateway request failed'));
+        }else {
+            $nextAction = $stripeIntents['next_action'];
+        }
+
+        switch ($this->config['payment_method']){
+            case "alipay":
+                if (isset($nextAction['alipay_handle_redirect'])){
+                    $jumpUrl = $nextAction['alipay_handle_redirect']['url'];
+                    $actionType = 1;
+                }else {
+                    throw new abort('unable get Alipay redirect url', 500);
+                }
+                break;
+            case "wechat_pay":
+                if (isset($nextAction['wechat_pay_display_qr_code'])){
+                    $jumpUrl = $nextAction['wechat_pay_display_qr_code']['data'];
+                }else {
+                    throw new abort('unable get WeChat Pay redirect url', 500);
+                }
+        }
+    } else {
+        $creditCheckOut = $stripe->checkout->sessions->create([
+            'success_url' => $order['return_url'],
+            'client_reference_id' => $order['trade_no'],
+            'payment_method_types' => ['card'],
+            'line_items' => [
+                [
+                    'price_data' => [
+                        'currency' => $currency,
+                        'unit_amount' => floor($order['total_amount'] * $exchange),
+                        'product_data' => [
+                            'name' => 'user-#' . $order['user_id'] . '-' . substr($order['trade_no'], -8),
+                        ]
+                    ],
+                    'quantity' => 1,
+                ],
+            ],
+            'mode' => 'payment',
+            'invoice_creation' => ['enabled' => true],
+            'phone_number_collection' => ['enabled' => false],
+            'customer_email' => $userEmail, 
+        ]);
+        $jumpUrl = $creditCheckOut['url'];
+        $actionType = 1;
+    }
 
         return [
             'type' => $actionType,
@@ -159,7 +152,7 @@ class StripeALL
                 $object = $event->data->object;
                 if ($object->status === 'succeeded') {
                     if (!isset($object->metadata->out_trade_no)) {
-                        return ('order error');
+                        return('order error');
                     }
                     $metaData = $object->metadata;
                     $tradeNo = $metaData->out_trade_no;
@@ -170,25 +163,25 @@ class StripeALL
                 }
                 break;
             case 'checkout.session.completed':
-                $object = $event->data->object;
-                if ($object->payment_status === 'paid') {
+                    $object = $event->data->object;
+                    if ($object->payment_status === 'paid') {
+                        return [
+                            'trade_no' => $object->client_reference_id,
+                            'callback_no' => $object->payment_intent
+                        ];
+                    }
+                    break;
+                case 'checkout.session.async_payment_succeeded':
+                    $object = $event->data->object;
                     return [
                         'trade_no' => $object->client_reference_id,
                         'callback_no' => $object->payment_intent
                     ];
-                }
-                break;
-            case 'checkout.session.async_payment_succeeded':
-                $object = $event->data->object;
-                return [
-                    'trade_no' => $object->client_reference_id,
-                    'callback_no' => $object->payment_intent
-                ];
-                break;
+                    break;
             default:
                 throw new abort('event is not support');
         }
-        return ('success');
+        return('success');
     }
     // 货币转换 API 1
     private function exchange($from, $to)
@@ -251,5 +244,11 @@ class StripeALL
             // 如果所有API都失败，抛出异常
             throw new \Exception("All currency conversion APIs failed.");
         }
+    }
+    // 从user中获取email
+    private function getUserEmail($userId)
+    {
+        $user = User::find($userId);
+        return $user ? $user->email : null;
     }
 }
