3ff7a39ecf9db9718a4025827b2d77c1322ba41c
wyx2685 <wyx2685@gmail.com>
2024-09-12 15:43:32 +0900

    fix 设备数限制未开启时获取在线数列表可能报错

--- app/Http/Controllers/V1/Server/UniProxyController.php
+++ app/Http/Controllers/V1/Server/UniProxyController.php
@@ -80,7 +80,9 @@ class UniProxyController extends Controller
     {
         $userService = new UserService();
         $users = $userService->getDeviceLimitedUsers();
-
+        if ($users->isEmpty()) {
+            return ;
+        }
         $cacheKeys = [];
         foreach ($users as $user) {
             $cacheKeys['ALIVE_IP_USER_' . $user->id] = $user->id;
